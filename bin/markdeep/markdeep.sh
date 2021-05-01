6a7d42ff8e2e6ee079b91c08e13ccd6155dd42b9
chenmo0 <53325264+chenmo0@users.noreply.github.com>
2021-05-01 22:04:08 +0800

    Delete markdeep.sh

--- bin/markdeep/markdeep.sh
+++
@@ -1,8 +0,0 @@
-file_name=~/bin/markdeep/a.html
-echo '<meta charset="utf-8">' > ${file_name}
-cat $1 >> ${file_name}
-echo '<style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style>' >> ${file_name}
-echo '<script src="~/bin/markdeep/markdeep.min.js" charset="utf-8"></script>' >> ${file_name}
-echo '<script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script>' >> ${file_name}
-echo '<script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>' >> ${file_name}
-google-chrome ${file_name}
