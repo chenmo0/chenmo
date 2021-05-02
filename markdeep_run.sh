f4cb3f0b679bbc20f5ae4dae3dc3e03cb7a1a746
chenmo0 <53325264+chenmo0@users.noreply.github.com>
2021-05-02 19:39:11 +0800

    Add files via upload

---
+++ markdeep_run.sh
@@ -0,0 +1,8 @@
+file_name=~/bin/markdeep/markdeep_show.html
+echo '<meta charset="utf-8">' > ${file_name}
+cat $1 >> ${file_name}
+echo '<style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style>' >> ${file_name}
+echo '<script src="'$HOME'/bin/markdeep/markdeep.min.js" charset="utf-8"></script>' >> ${file_name}
+echo '<script src="https://morgan3d.github.io/markdeep/latest/markdeep.min.js" charset="utf-8"></script>' >> ${file_name}
+echo '<script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>' >> ${file_name}
+google-chrome-stable ${file_name}
